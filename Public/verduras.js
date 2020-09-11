9da9c3c3beec18eed1517ec2e20c69bc92adf68f
Nicolas <ndiazkolmann@gmail.com>
2020-09-11 19:14:15 -0300

    calculo de precios y cantidad

---
+++ Public/verduras.js
@@ -0,0 +1,17 @@
+const verduras = {
+
+    pera: {
+        nombre: 'pera',
+        precio: 2,
+        peso: 22
+    },
+
+    morron: {
+        nombre: 'morron',
+        precio: 4,
+        peso: 44
+    }
+
+}
+
+module.exports = verduras;
