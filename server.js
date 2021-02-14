b21763df598d0665b2ef8f5b0e60d04847267217
nd185059 <nd185059@ncr.com>
2021-02-14 20:54:11 -0300

    a

--- server.js
+++ server.js
@@ -14,16 +14,16 @@ app.use(express.urlencoded({extended:false}));  //JSON
 app.use('/', rutas );
 
 // mongoose
-mongoose.connect('mongodb://localhost:27017/perros', {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Conectado a MongoDB');
-  console.log('***************************************************');
-});
+// mongoose.connect('mongodb://localhost:27017/perros', {
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true
+// });
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('Conectado a MongoDB');
+//   console.log('***************************************************');
+// });
 
 
 // runing
